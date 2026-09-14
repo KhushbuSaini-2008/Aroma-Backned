db2514f8ae1beb78e8c5a9d9d6b4eeb4ea4ace65
KhushbuSaini-2008 <khushbsaini24.set@modyuniversity.ac.in>
2026-09-14 12:24:53 +0530

    new file

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ connectDB();
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "*", credentials: false }));
+app.options("*", cors());
 app.use(express.json());
 
 // Routes
